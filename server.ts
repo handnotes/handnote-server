4436be7bea6e2032d00e9281abe2c51367e38306
mutoe <mutoe@foxmail.com>
2019-06-28 09:16:15 +0800

    feat: 允许在开发模式下启动本地服务器了

--- server.ts
+++ server.ts
@@ -11,8 +11,11 @@ import createConnection from './database.connection'
   /** Application working port */
   const PORT: number = Number(process.env.APP_PORT) || 3050
 
+  /** Application listener host (for local area network) */
+  const HOSTNAME = process.env.NODE_ENV === 'development' ? '0.0.0.0' : '127.0.0.1'
+
   // launch App
-  app.listen(PORT, () => {
+  app.listen(PORT, HOSTNAME, () => {
     console.log(`koa is started on ${PORT}`)
   })
 })()
