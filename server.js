841b292e65f9b14c193097ed3428152ad2ade867
mutoe <mutoe@foxmail.com>
2019-08-02 09:42:15 +0800

    Revert "chore: setting sonar config"
    This reverts commit 9625e62813dcafc3c76ccd9536300b4ee10c19ec.

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-
-const server = http.createServer((req, res) => {
-	console.log(req.url)
-	console.log('received')
-	res.end('hello')
-})
-server.listen(4000)
-console.log('start success')
