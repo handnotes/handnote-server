e3fbbb869539e09ec0ea5a30253641c705087b6b
gethin.yan <gethin.yan@gmail.com>
2019-08-13 10:00:56 +0800

    format long code

--- models/models.go
+++ models/models.go
@@ -15,7 +15,9 @@ var dbConn *gorm.DB
 func init() {
 	var err error
 
-	connStr := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=%s password=%s", setting.Database.Host, setting.Database.Port, setting.Database.User, setting.Database.Dbname, setting.Database.Sslmode, setting.Database.Password)
+	connStr := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=%s password=%s",
+		setting.Database.Host, setting.Database.Port, setting.Database.User,
+		setting.Database.Dbname, setting.Database.Sslmode, setting.Database.Password)
 	dbConn, err = gorm.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalln(err)
