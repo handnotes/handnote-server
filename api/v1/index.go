c1131ec8af2dd00438bb2970f511618887cbbf74
gethin.yan <gethin.yan@gmail.com>
2019-08-17 15:21:31 +0800

    modify email content

--- api/v1/index.go
+++ api/v1/index.go
@@ -18,11 +18,11 @@ func Hello(c *gin.Context) {
 
 // SendEmail send email from handnote to destination email address.
 func SendEmail(c *gin.Context) {
-	from := mail.NewEmail("Example User Gmail", "gethin.yan@gmail.com")
-	subject := "Sending with Twilio SendGrid is Fun"
-	to := mail.NewEmail("Example User Foxmail", "gethin.yan@foxmail.com")
-	plainTextContent := "and easy to do anywhere, even with Go"
-	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+	from := mail.NewEmail("Handnote", "no_reply@gethin.cn")
+	subject := "Handnote注册验证码"
+	to := mail.NewEmail("gethin.yan", "gethin.yan@foxmail.com")
+	plainTextContent := "Handnote注册验证码"
+	htmlContent := "<p>尊敬的Handnote用户：</p><p>您好!</p><p>您的注册验证码是：123456。</p>"
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
 	fmt.Println(os.Getenv("SENDGRID_API_KEY"))
 	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
