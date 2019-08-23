1d3b0c89575a96f84674339b67e59d7d669ee8c1
gethin.yan <gethin.yan@gmail.com>
2019-08-23 10:20:02 +0800

    change the way to get token

--- middleware/jwt/jwt.go
+++ middleware/jwt/jwt.go
@@ -3,7 +3,6 @@ package jwt
 import (
 	"fmt"
 	"net/http"
-	"strings"
 
 	"e.coding.net/handnote/handnote/pkg/util"
 	"github.com/gin-gonic/gin"
@@ -12,17 +11,17 @@ import (
 // JWT is jwt middleware
 func JWT() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		token := c.Request.Header.Get("Authorization")
-		splitToken := strings.Split(token, "Bearer")
-		if len(splitToken) != 2 || splitToken[1] == "" {
+		header := c.Request.Header.Get("Authorization")
+		// Parse the header to get the token part
+		var token string
+		fmt.Sscanf(header, "Bearer %s", &token)
+		if token == "" {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"message": "StatusBadRequest",
 			})
 			c.Abort()
 			return
 		}
-		token = strings.TrimSpace(splitToken[1])
-		fmt.Println(token)
 		user, err := util.ParseToken(token)
 		fmt.Println(user)
 		if err != nil {
